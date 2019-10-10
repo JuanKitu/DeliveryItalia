a862084549ecb8abfa9b801f4a13ecf5a1755290
LucasMartinPrado <prado.lucasm@gmail.com>
2019-10-10 16:40:51 -0300

    Upload inicial

---
+++ HabProf/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrincipalComponent,
+    NavigationBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

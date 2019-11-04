bc8b0e442e36f5fdfbeba659131f8abf15c24b12
JuanKitu <juanmascruz@gmail.com>
2019-11-03 23:12:33 -0300

    basic server configuration implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,21 @@
 //ayu dark theme
+const express = require('express');
+const {json} =require('express');
+const morgan = require('morgan');
+colors = require('colors');
+const app = express();
+
+/*--- Settings ---*/
+app.set('json spaces',2);
+app.set('port',3000);
+/*--- importing routes ---*/
+
+/*--- middleware ---*/
+app.use(morgan('dev'));
+app.use(json());
+/*--- Routes ---*/
+
+/*--- Starup ---*/
+app.listen(app.get('port'),()=>{
+    console.log(`Server on port: ${app.get('port')}`.magenta);
+});

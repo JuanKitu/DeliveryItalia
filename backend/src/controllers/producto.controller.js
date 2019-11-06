faca406866fbc4146dd7000cb71db6b3843e9d46
JuanKitu <38724038+JuanKitu@users.noreply.github.com>
2019-11-05 21:47:13 -0300

    controller of productos has benn updated

--- backend/src/controllers/producto.controller.js
+++ backend/src/controllers/producto.controller.js
@@ -3,49 +3,126 @@ Producto = require('../models/Producto.js');
 
 
 /*--- Create a product ---*/
-controller.new = async (req,res)=>{
-    const {nombre,descripcion,precios} = req.body;
-    try{
+controller.new = async (req, res) => {
+    const { nombre, descripcion, precios } = req.body;
+    try {
         const newProducto = await Producto.create({
             nombre,
             descripcion,
             precios
-        },{
-            fields:['nombre','descripcion','precios']
+        }, {
+            fields: ['nombre', 'descripcion', 'precios']
         });
-        if(newProducto){
+        if (newProducto) {
             return res.json({
-                message:'The Productos has been created',
-                data:newProducto
+                message: 'The Productos has been created',
+                data: newProducto
             })
         }
-    }catch(error){
+    } catch (error) {
         console.log(error);
         return res.json({
-            error:'The server has been problems',
-            data:{}
+            error: 'The server has been problems',
+            data: {}
         })
 
     };
 };
-
 /*--- Query of products ---*/
-controller.getAll = async (req,res)=>{
-    try{
+controller.getAll = async (req, res) => {
+    try {
         const productos = await Producto.findAll();
         return res.json({
             data: productos
         });
-    }catch(error){
+    } catch (error) {
         console.log(error);
         return res.json({
             error: 'The server has been error'
         });
-    };    
-}
+    };
+};
+/*--- Edit a product ---*/
+controller.change = async (req, res) => {
+    try{
+        const {idProducto} = req.params;
+        const {nombre,descripcion,precios} = req.body;
+        const newProducto = await Producto.update({
+            nombre,
+            descripcion,
+            precios
+        },
+        {
+            where: {
+                idProducto
+            }
+        });
+        const producto = await Producto.findOne({
+            attributes:['idProducto','nombre','descripcion','precios'],
+            where:{
+                idProducto
+            }
+           
+        });
+        return res.json({
+            message:'The Producto has been updated',
+            data:producto
+        });
+    }catch(error){
+        console.log(error);
+        return res.json({
+            error:'The server has been error',
+            data:{}
+        });
+    };
 
+};
+/*--- Delete a product ---*/
+controller.delete = async (req, res) => {
+    try{
+        const {idProducto} = req.params;
+        const deleteRowCount = await Producto.destroy({
+            where:{
+                idProducto
+            }
+        });
+        return res.json({
+            message:'The Producto has been deleted',
+            count:deleteRowCount
+        });
 
+    }catch(error){
+        console.log(error);
+        return res.json({
+            error:'The server has been error',
+            data:{}
+        });
+    };
 
+};
+/*--- Find a product ---*/
+controller.getById = async (req, res) => {
+    try {
+        const { idProducto } = req.params;
+        const aProducto = await Producto.findOne({
+            where: {
+                idProducto
+            },
+            attributes: ['idProducto', 'nombre', 'descripcion', 'precios']
+        });
+        return res.json({
+            data:aProducto
+        })
+    }catch(error){
+        console.log(error);
+        return res.json({
+            error:'The server has been error',
+            data:{}
+        });
 
+    };
+    
+
+};
 
 module.exports = controller;

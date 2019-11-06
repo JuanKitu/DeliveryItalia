8e740635d4094aa13cff9435b871fcde0e20d11b
JuanKitu <38724038+JuanKitu@users.noreply.github.com>
2019-11-06 15:32:50 -0300

    Update categoriaGusto.js

--- backend/src/routes/categoriaGusto.js
+++ backend/src/routes/categoriaGusto.js
@@ -3,8 +3,8 @@ const router = Router();
 const controller = require('../controllers/categoriaGusto.controller.js');
 
 router.get('/',controller.getAll);
-router.get('/:idProducto',controller.getById);
+router.get('/:idCategoria',controller.getById);
 router.post('/',controller.new);
-router.delete('/:idProducto',controller.delete);
-router.put('/:idProducto',controller.change);
+router.delete('/:idCategoria',controller.delete);
+router.put('/:idCategoria',controller.change);
 module.exports = router;
